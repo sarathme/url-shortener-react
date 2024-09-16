17d3ada49005b92960ceef00ac2b397f80a79f5a
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-16 22:11:54 +0530

    Define routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import NotFound from "./pages/NotFound";
+import { Toaster } from "react-hot-toast";
+import ResetPassword from "./pages/ResetPassword";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="app" />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="forgotPassword" element={<ForgotPassword />} />
+          <Route path="resetPassword/:resetToken" element={<ResetPassword />} />
+          <Route path="app" element={<h1>App Layout</h1>}></Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
